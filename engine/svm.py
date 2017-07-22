1a78b1c062d18124b717306721699f39016b3dc4
jjmachan <jamesjithin97@gmail.com>
2017-07-22 13:18:11 +0530

    Add links for desha

---
+++ engine/svm.py
@@ -0,0 +1,125 @@
+import string
+
+from nltk.corpus import stopwords as sw
+from nltk.corpus import wordnet as wn
+from nltk import wordpunct_tokenize
+from nltk import WordNetLemmatizer
+from nltk import sent_tokenize
+from nltk import pos_tag
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import classification_report as clsr
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cross_validation import train_test_split as tts
+import timeit
+
+
+class NLTKPreprocessor(BaseEstimator, TransformerMixin):
+
+    def __init__(self, stopwords=None, punct=None,
+                 lower=True, strip=True):
+        self.lower      = lower
+        self.strip      = strip
+        self.stopwords  = stopwords or set(sw.words('english'))
+        self.punct      = punct or set(string.punctuation)
+        self.lemmatizer = WordNetLemmatizer()
+
+    def fit(self, X, y=None):
+        return self
+
+    def inverse_transform(self, X):
+        return [" ".join(doc) for doc in X]
+
+    def transform(self, X):
+        return [
+            list(self.tokenize(doc)) for doc in X
+        ]
+
+    def tokenize(self, document):
+        # Break the document into sentences
+        for sent in sent_tokenize(document):
+            # Break the sentence into part of speech tagged tokens
+            for token, tag in pos_tag(wordpunct_tokenize(sent)):
+                # Apply preprocessing to the token
+                token = token.lower() if self.lower else token
+                token = token.strip() if self.strip else token
+                token = token.strip('_') if self.strip else token
+                token = token.strip('*') if self.strip else token
+
+                # If stopword, ignore token and continue
+                if token in self.stopwords:
+                    continue
+
+                # If punctuation, ignore token and continue
+                if all(char in self.punct for char in token):
+                    continue
+
+                # Lemmatize the token and yield
+                lemma = self.lemmatize(token, tag)
+                yield lemma
+
+    def lemmatize(self, token, tag):
+        tag = {
+            'N': wn.NOUN,
+            'V': wn.VERB,
+            'R': wn.ADV,
+            'J': wn.ADJ
+        }.get(tag[0], wn.NOUN)
+
+        return self.lemmatizer.lemmatize(token, tag)
+
+def build_and_evaluate(X, y,
+    classifier=SGDClassifier, outpath=None, verbose=True):
+
+    def build(classifier, X, y=None):
+        """
+        Inner build function that builds a single model.
+        """
+        if isinstance(classifier, type):
+            classifier = classifier()
+
+        model = Pipeline([
+            ('preprocessor', NLTKPreprocessor()),
+            ('vectorizer', TfidfVectorizer(
+                tokenizer=identity, preprocessor=None, lowercase=False
+            )),
+            ('classifier', classifier),
+        ])
+
+        model.fit(X, y)
+        return model
+
+    # Label encode the targets
+    labels = LabelEncoder()
+    y = labels.fit_transform(y)
+
+    # Begin evaluation
+    if verbose: print("Building for evaluation")
+    X_train, X_test, y_train, y_test = tts(X, y, test_size=0.2)
+    model, secs = build(classifier, X_train, y_train)
+
+    if verbose:
+        print("Evaluation model fit in {:0.3f} seconds".format(secs))
+        print("Classification Report:\n")
+
+    y_pred = model.predict(X_test)
+    print(clsr(y_test, y_pred, target_names=labels.classes_))
+
+    if verbose:
+        print("Building complete model and saving ...")
+    model, secs = build(classifier, X, y)
+    model.labels_ = labels
+
+    if verbose:
+        print("Complete model fit in {:0.3f} seconds".format(secs))
+
+    if outpath:
+        with open(outpath, 'wb') as f:
+            pickle.dump(model, f)
+
+        print("Model written out to {}".format(outpath))
+
+    return model
--- makri-links.py
+++ makri-links.py
@@ -12,17 +12,51 @@ class MakriSpider(scrapy.Spider):
     name = "makri_spider"
     urls = [
         'http://www.deshabhimani.com/news/national',
+        'http://www.deshabhimani.com/news/kerala',
+        'http://www.deshabhimani.com/news/national',
+        'http://www.deshabhimani.com/news/world',
+        'http://www.deshabhimani.com/editorial',
+        'http://www.deshabhimani.com/articles',
+        'http://www.deshabhimani.com/columns',
+        'http://www.deshabhimani.com/news/sports',
+        'http://www.deshabhimani.com/cinema',
+        'http://www.deshabhimani.com/from-the-net',
+        'http://www.deshabhimani.com/art-stage',
+        'http://www.deshabhimani.com/epaper/',
+        'http://www.deshabhimani.com/news/pravasi',
+        'http://www.deshabhimani.com/education',
+        'http://www.deshabhimani.com/health',
+        'http://www.deshabhimani.com/news/vehicle',
+        'http://www.deshabhimani.com/books',
+        'http://www.deshabhimani.com/news/technology',
+        'http://www.deshabhimani.com/music',
+        'http://www.deshabhimani.com/travel',
+        'http://www.deshabhimani.com/women',
+        'http://www.deshabhimani.com/life-style',
+        'http://www.deshabhimani.com/news/business',
+        'http://www.deshabhimani.com/agriculture',
+        'http://www.deshabhimani.com/cartoon',
+        'http://www.deshabhimani.com/news/career',
     ]
 
     def start_requests(self):
-        # with open("test.txt", "rb") as fp:
-        #     urls = pickle.load(fp)
-        # print(urls)
-        for url in self.urls[:1]:
+        with open("test1.txt", "wb") as fp:
+            pickle.dump([], fp)
+        for url in self.urls[:3]:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        soup = BeautifulSoup(response.body,"lxml")
+        links = []
+        with open("test1.txt", "rb") as fp:
+            links = pickle.load(fp)
+        soup = BeautifulSoup(response.body, "lxml")
         links1 = soup.findAll("div", {"class": "lead-news"})
-        links2 = soup.findAll("div", {"class": "db-bx"})
-        print(links2)
+        links2 = soup.find("div", {"class": "db-bx"}).findAll("li")
+        for link in links1:
+        	links.append(link.find("a").attrs['href'])
+        for i, link in enumerate(links2):
+        	if i<10:
+        		links.append(link.find("a").attrs['href'])
+        print(links)
+        with open("test1.txt", "wb") as fp:
+            pickle.dump(links, fp)
